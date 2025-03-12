599a46a7d0b4dc66643b7369359015e2b67507ae
Kafi <ahkafi05@gmail.com>
2025-03-12 16:24:07 +0600

    modified video card

--- script.js
+++ script.js
@@ -8,24 +8,51 @@ const fetchAllVideos = () =>{
 }
 fetchAllVideos();
 
-const showVideos = (videosData) =>{
-    videosData.forEach(videoData => {
-        const videoThumbnail = videoData.thumbnail;
-        const videoTitle = videoData.title;
-        const videoCategory = videoData.category_id;
+const showVideos = (video) =>{
+    video.forEach(video => {
+        const videoCategory = video.category_id;
+        // {
+        //     "category_id": "1003",
+        //     "video_id": "aaae",
+        //     "thumbnail": "https://i.ibb.co/Yc4p5gD/inside-amy.jpg",
+        //     "title": "Inside Amy Schumer",
+        //     "authors": [
+        //     {
+        //     "profile_picture": "https://i.ibb.co/YD2mqH7/amy.jpg",
+        //     "profile_name": "Amy Schumer",
+        //     "verified": ""
+        //     }
+        //     ],
+        //     "others": {
+        //     "views": "3.6K",
+        //     "posted_date": "15147"
+        //     },
+        console.log(video.authors[0].profile_name, 1)
 
         const cardDiv = document.createElement("div");
-        console.log(videoThumbnail)
         cardDiv.innerHTML = 
         `
-        <div class="card bg-base-100 shadow-sm">
-        <figure>
-            <img class="object-cover h-50 w-full" src=${videoThumbnail} alt="Shoes" />
-        </figure>
-        <div class="card-body">
-            <h2 class="card-title">${videoTitle}</h2>
-        </div>
-        </div>
+                <div class="card bg-base-100 ">
+                    <figure class="relative">
+                        <img class="object-cover h-50 w-full rounded-md" src="${video.thumbnail}" alt="Shoes" />
+                        <p class="absolute text-white bg-gray-900 px-2 py-1 rounded-sm bottom-2 right-2 text-sm font-semibold text-center">3hrs 50min ago</p>
+                    </figure>
+                    <div class="card-body flex flex-row px-0 items-start">
+                        <div class="avatar pt-1">
+                            <div class="ring-primary ring-offset-base-100 w-8 rounded-full ring ring-offset-2">
+                              <img src=${video.authors[0].profile_picture}/>
+                            </div>
+                          </div>
+                        <div>
+                            <h2 class="card-title ml-2 mb-1">${video.title}</h2>
+                            <div class="flex items-center justify-start">
+                                <span class="mx-2 text-[#171717B3] mb-1">${video.authors[0].profile_name}</span>
+                                <img class="h-4" src="https://img.icons8.com/?size=100&id=2AuMnRFVB9b1&format=png&color=000000" alt="">
+                            </div>
+                            <p class="ml-2 text-[#171717B3] mb-1">${video.others.views} views</p>
+                        </div>
+                    </div>
+                </div>
         `
         document.getElementById("video-section").append(cardDiv);
     });
